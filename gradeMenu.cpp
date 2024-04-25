5db9fb9e8e174133847f8f12f2459022f2d6d8c4
Parnell <113159866+PThrower@users.noreply.github.com>
2024-04-25 15:49:04 -0700

    Menu And Initial Files Done

--- gradeMenu.cpp
+++ gradeMenu.cpp
@@ -8,30 +8,35 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include "taskMenu.h"
 #include "menu.h"
-#include "dateTime.h"
-#include "taskList.h"
+//#include "gradeMenu.h"
+#include "gradeMenu.h"
 
 using namespace std;
 
-TaskMenu::TaskMenu(unordered_map<string, Task>& tasks) : tasks(tasks), Menu("Main Menu") {
-	addOption("a) Add task");
-	addOption("e) Edit task");
-	addOption("d) Delete task");
-	addOption("p) List pending tasks");
-	addOption("c) List completed tasks");
+GradeMenu::GradeMenu() {
+	addOption("1) List");
+	addOption("2) View");
+	addOption("3) Add");
+	addOption("4) Edit");
+	addOption("5) Remove");
+	addOption("6) Calculate Grade");
+	addOption("7) Save");
 	addOption("x) Exit");
 
-	list = new TaskList();
+	//list = new TaskList();
 	init();
 }
-TaskMenu::~TaskMenu() {	
-	delete list;
+GradeMenu::~GradeMenu() {	
+	//delete list;
 	inFile.close();
 }
 
-void TaskMenu::init() {
+void GradeMenu::init() {
+
+}
+
+/*void GradeMenu::init() {
     inFile.open(TASK_DATA);
 
     string text;
@@ -44,23 +49,23 @@ void TaskMenu::init() {
             continue;
         }
         stringstream ss(line);
-        Task task;
+        //Task task;
         getline(ss, text, ',');
-        task.setTerm(text);
+        //task.setTerm(text);
         getline(ss, text, ',');
-        task.setName(text);
+        //task.setName(text);
         getline(ss, text, ',');
-        task.setStartDate(text);
+        //task.setStartDate(text);
         getline(ss, text, ',');
-        task.setEndDate(text);
+       // task.setEndDate(text);
         getline(ss, text, ',');
-        task.setStatus(stoi(text));    // value =1 means DONE! and value = 0 is pending
+        //task.setStatus(stoi(text));    // value =1 means DONE! and value = 0 is pending
 
         // Add the task to the map
-        tasks.insert({task.getName(), task});
+        //tasks.insert({task.getName(), task});
     }
     inFile.close();
-}
+}*/
 
 /*void TaskMenu::init() {
 	inFile.open(TASK_DATA);
@@ -91,53 +96,67 @@ void TaskMenu::init() {
 	inFile.close();
 }*/
 
-void TaskMenu::viewPendingTasks() {
+void GradeMenu::doList() {
 	showOption(getName(3).substr(3));
-	list->printTable(false);
+	//list->printTable(false);
 }
 
-void TaskMenu::viewCompletedTasks() {
+void GradeMenu::doView() {
 	showOption(getName(4).substr(3));
-	list->printTable(true);
+	//list->printTable(true);
 }
 
-void TaskMenu::addNewTask() {
+void GradeMenu::doAdd() {
 	showOption(getName(0).substr(3));
-	list->addNew();
+	//list->addNew();
 	cout << endl;
 }
 
-void TaskMenu::editTask() {
+void GradeMenu::doEdit() {
 	showOption(getName(1).substr(3));
-	list->editTask();
+	//list->editTask();
 	cout << endl;
 }
 
-void TaskMenu::deleteTask() {
+void GradeMenu::doRemove() {
 	showOption(getName(2).substr(3));	
-	list->deleteTask();
+	//list->deleteTask();
 	cout << endl;
 }
 
-void TaskMenu::activate() {
+void GradeMenu::calculateGrade() {
+
+}
+
+void GradeMenu::doSave() {
+
+}
+
+void GradeMenu::activate() {
 	char command = COMMAND::ADD;
 	while (command != EXIT) {
 		command = doMenuOption();
 		switch (command) {		
+		case LIST:
+			doList();
+			break;
+		case VIEW:
+			doView();
+			break;
 		case ADD:
-			addNewTask();
+			doAdd();
 			break;
 		case EDIT:
-			editTask();
+			doEdit();
 			break;
-		case DELETE:
-			deleteTask();
+		case REMOVE:
+			doRemove();
 			break;
-		case PENDING:
-			viewPendingTasks();
+		case CALCULATE:
+			calculateGrade();
 			break;
-		case DONE:
-			viewCompletedTasks();
+		case SAVE:
+			doSave();
 			break;
 		case EXIT:
 			cout << "Bye!" << endl;
@@ -149,6 +168,6 @@ void TaskMenu::activate() {
 	}	
 }
 
-void TaskMenu::showOption(const string title) {
+void GradeMenu::showOption(const string title) {
 	cout << "***** " << title << " *****" << endl;
 }
